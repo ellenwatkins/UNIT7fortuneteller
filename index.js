50e02bbab1e7f07d578019bab58ecbed699e4ca5
Ellen Watkins <ellen@scripted.org>
2017-11-03 15:33:17 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Welcome to the Fortune Teller!
+
+// Directions: 
+// Follow the instructions below to make the age and adjective appear in the fortune when you click the button.
+
+var birthYear;
+var age;
+var adjective;
+
+$(".btn").click(function(){
+   birthYear = $("#birth-year").val(); 
+   //Add code here to calculate your age in 2050
+   
+   
+   
+   $(".age").html(age);
+   
+   //Bonus! Add code below to make the adjectives appear in the fortune
+});
+
+// Double Bonus! Try setting up your own input box, variable and jQuery to take in a verb and make it apear on the page.
+
